4e8c6a9de6327c92cfac8b5322e257c599b00f20
jbortiz0623 <jbortiz0623@gmail.com>
2018-11-26 10:35:28 -0800

    Update project00.js

--- starter-code/scripts/project00.js
+++ starter-code/scripts/project00.js
@@ -1,27 +1,68 @@
-// var jq = jQuery.noConflict();
-//
-// jq.ajax({
-//   key: 'd15fe3818a34b197a285e572b80a7300'
-//   url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&api_key=d15fe3818a34b197a285e572b80a7300',
-//   method: 'GET',
-//   success: function(response) {
-//
-//     jq('#main').empty();
-//     var cities = response.results
-//
-//     console.log(cities)
-//             cities.forEach(radio => {
-//
-//                 let elem = `<p>${radio.} population is ${radio.} and has ${radio.} </p><br>`
-//                 console.log();
-//
-//                 jq('#main').append(elem)
-//             });
-//   },
-//
-//
-//
-//     error: function(error) {
-//
-// }
-// });
+$(document).ready(function(){
+$("button").click(function(){
+  $("#div1").fadeOut();
+  $("#div2").fadeOut("slow");
+  $("#div3").fadeOut(3000);
+  $("#div4").fadeOut(4000);
+});
+});
+
+var cars = ["HTML", "CSS", "Bootstrap", "SASS", "Flexbox", "GRID", "JavaScript", "Ajax", "jQuery", "Node.js", "SQL", "IoT"];
+var text = "";
+var i;
+for (i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+
+
+
+
+
+<!-- #region Jssor Slider Begin -->
+
+        jQuery(document).ready(function ($) {
+
+            var jssor_1_options = {
+              $AutoPlay: 1,
+              $AutoPlaySteps: 5,
+              $SlideDuration: 160,
+              $SlideWidth: 200,
+              $SlideSpacing: 3,
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,
+                $Steps: 5
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*#region responsive code begin*/
+
+            var MAX_WIDTH = 980;
+
+            function ScaleSlider() {
+                var containerElement = jssor_1_slider.$Elmt.parentNode;
+                var containerWidth = containerElement.clientWidth;
+
+                if (containerWidth) {
+
+                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+                    jssor_1_slider.$ScaleWidth(expectedWidth);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            /*#endregion responsive code end*/
+        });
